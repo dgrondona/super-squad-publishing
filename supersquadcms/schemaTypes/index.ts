9f83a96709ed9359cc8c3bce0c54d2d031c6f712
dgrondona <draven.grondona@gmail.com>
2025-11-28 17:48:35 -0800

    added artists page through sanity

--- supersquadcms/schemaTypes/index.ts
+++ supersquadcms/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import artist from './artist'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, artist]
